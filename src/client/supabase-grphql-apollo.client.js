5d1d8f49a76b7a6ca75364e915c6b874a6e88f58
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2026-04-17 09:07:40 +0530

    feat: integrate Supabase client and enhance Apollo Client configuration

--- src/client/supabase-grphql-apollo.client.js
+++ src/client/supabase-grphql-apollo.client.js
@@ -1,26 +1,42 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, defaultDataIdFromObject } from "@apollo/client";
 import { SetContextLink } from "@apollo/client/link/context";
+import supabase from "./supabase";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY; 
+
 
 const httpLink = new HttpLink({
   uri: `${SUPABASE_URL}/graphql/v1`, // The GraphQL endpoint
 });
 
-const authLink = new SetContextLink((_, { headers }) => {
-  // Return the headers to the context so httpLink can read them
+const authLink = new SetContextLink(async (_, { headers }) => {
+  // runs before every request
+  
+  const token = (await supabase.auth.getSession()).data.session?.access_token;
+
   return {
     headers: {
       ...headers,
-      apiKey: SUPABASE_ANON_KEY,
+      Authorization: token ? `Bearer ${token}` : '',
+      apikey: SUPABASE_ANON_KEY,
     }
   };
 });
 
+const cache = new InMemoryCache({
+    dataIdFromObject(responseObject){
+      if('nodeId' in responseObject){
+        return `${responseObject.nodeId}`;
+      }
+
+      return defaultDataIdFromObject(responseObject);
+    }
+  });
+
 const apolloSupabaseGraphqlClient = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  cache
 });
 
 export default apolloSupabaseGraphqlClient;
