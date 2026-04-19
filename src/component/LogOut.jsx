b0045d13523955a5f3135b38d5e11c214f069e9e
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2026-04-19 19:13:53 +0530

    feat: implement authentication context and role-based routing; refactor app structure

--- src/component/LogOut.jsx
+++ src/component/LogOut.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import {LogoutOutlined} from "@ant-design/icons";
-import { Typography } from 'antd';
+import { LogoutOutlined } from "@ant-design/icons";
+import { Button, Typography } from 'antd';
+import { useAuth } from '../const/functions';
 const { Text } = Typography;
 
 function LogOut() {
+
+  const { signOut } = useAuth();
+
   return (
     <div className='absolute bottom-0 left-0 w-full px-10 py-6'>
-        <div className='flex items-center gap-2 cursor-pointer' style={{ color: '#fff' }}>
-            <LogoutOutlined />
-            <Text style={{color: '#fff',fontSize:16}}>Logout</Text>
-            </div>
+      <div onClick={signOut} className='flex items-center gap-2 cursor-pointer' style={{ color: '#fff' }}>
+        <LogoutOutlined />
+        <Text style={{ color: '#fff', fontSize: 16 }}>Logout</Text>
+      </div>
     </div>
   )
 }
